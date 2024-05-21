23ade37f2e289f33515016dac0acb14b9df94901
sanjeevan <s.y.gunasekaran@accenture.com>
2024-05-21 20:19:31 +0530

    added navigation and initial pages

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,20 +1,25 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./index.css";
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import Pricing from "./pages/Pricing";
 import Product from "./pages/Product";
-import PageNav from "./components/PageNav";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
+
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <PageNav />
         <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/product" element={<Product />} />
-          <Route path="/pricing" element={<Pricing />} />
-          <Route path="*" element={<PageNotFound />} />
+          <Route path="/" element={ <Homepage /> } />
+          <Route path="product" element={ <Product /> } />
+          <Route path="pricing" element={ <Pricing /> } />
+          <Route path="app" element={ <AppLayout /> } />
+          <Route path="login" element={<Login /> } />
+          <Route path="*" element={ <PageNotFound /> } />
+          
         </Routes>
       </BrowserRouter>
     </>
---
+++ worldwise/src/pages/AppLayout.jsx
@@ -0,0 +1,7 @@
+import styles from "./AppLayout.module.css"
+
+export default function AppLayout() {
+    return (
+        <div className={styles.app}>AppLayout</div>
+    )
+}
