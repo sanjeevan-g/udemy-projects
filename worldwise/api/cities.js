e4c020e1093dce86b12aacf55c8f1f2757234fc2
sanjeevan <s.y.gunasekaran@accenture.com>
2024-08-22 20:11:38 +0530

    file updates

--- worldwise/api/cities.js
+++ worldwise/api/cities.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 
-const filePath = path.join(process.cwd(), "src","data", "cities.json");
+const filePath = path.join(process.cwd(), "public", "cities.json");
 // console.log(filePath)
 const writePath = path.join("/tmp", "cities.json");
 
