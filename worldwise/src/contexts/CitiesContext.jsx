92cbf5097aad7546a5dfbb792697044479e88aab
sanjeevan <s.y.gunasekaran@accenture.com>
2024-08-23 11:39:08 +0530

    context update

--- worldwise/src/contexts/CitiesContext.jsx
+++ worldwise/src/contexts/CitiesContext.jsx
@@ -1,188 +1,148 @@
-/* eslint-disable react/prop-types */
-import { createContext, useCallback, useContext, useEffect, useReducer } from "react";
+import {
+    createContext,
+    useEffect,
+    useContext,
+    useReducer,
+    useCallback,
+} from "react";
+import PropTypes from "prop-types";
 
-// const BASE_URL = "http://localhost:4200";
-const BASE_URL = "https://worldwise-one-tau.vercel.app/api"
-
-const CitiesContext = createContext();
-
-function reducer(state, action) {
-    switch (action.type) {
-        case "loading": {
-            return {
-                ...state,
-                isLoading: true
-            }
-        }
-
-        case 'cities_loaded': {
-            return {
-                ...state,
-                isLoading: false,
-                cities: action.payload
-            }
-        }
-
-        // for current city
-        case 'city_loaded': {
-            return {
-                ...state,
-                isLoading: false,
-                currentCity: action.payload
-            }
-        }
-
-        case 'city_created': {
-            return {
-                ...state,
-                isLoading: false,
-                cities: [...state.cities, action.payload],
-                currentCity: action.payload
-            }
-        }
-
-        case 'cities_deleted': {
-            return {
-                ...state,
-                isLoading: false,
-                currentCity: {},
-                cities: state.cities.filter(city => city.id !== action.payload)
-            }
-        }
-
-        case "rejected": {
-            return {
-                ...state,
-                error: action.payload,
-                isLoading: false
-            }
-        }
-    }
-}
+const citiesContext = createContext();
 
 const initialState = {
     cities: [],
     isLoading: false,
-    currentCity: {},
-    error: ""
+    curCity: {},
+    errorMsg: "",
+};
+
+function reducer(state, action) {
+    switch (action.type) {
+        case "loading":
+            return { ...state, isLoading: true };
+        case "cities/loaded":
+            return { ...state, cities: action.payLoad, isLoading: false };
+        case "city/loaded":
+            return { ...state, curCity: action.payLoad, isLoading: false };
+        case "city/created":
+            return {
+                ...state,
+                cities: [...state.cities, action.payLoad],
+                curCity: action.payLoad,
+                isLoading: false,
+            };
+        case "city/deleted":
+            return {
+                ...state,
+                cities: state.cities.filter((city) => city.id !== action.payLoad),
+                isLoading: false,
+            };
+        case "rejected":
+            return { ...state, isLoading: false, errorMsg: action.payLoad };
+        default:
+            throw new Error("Action type not found");
+    }
 }
 
+function CitiesProvider({ children }) {
+    const [{ cities, isLoading, curCity, errorMsg }, dispatch] = useReducer(
+        reducer,
+        initialState
+    );
 
-export function CitiesProvider({ children }) {
-
-    const [{ cities, isLoading, currentCity, error }, dispatch] = useReducer(reducer, initialState)
-
-    // const [cities, setCities] = useState([]);
-    // const [isLoading, setIsLoading] = useState(false);
-    // const [currentCity, setCurrentCity] = useState({})
-
-
-    useEffect(() => {
-        async function fetchCities() {
+    useEffect(function () {
+        async function getCities() {
             dispatch({ type: "loading" });
             try {
-                const res = await fetch(`${BASE_URL}/cities`);
+                const res = await fetch("https://worldwise-one-tau.vercel.app/api/cities");
+                if (!res.ok) throw new Error("Something went wrong");
                 const data = await res.json();
                 dispatch({
-                    type: "cities_loaded",
-                    payload: data
-                })
-
-            } catch (e) {
-                dispatch({
-                    type: "rejected",
-                    payload: "there was an error fetching cities...."
-                })
+                    type: "cities/loaded",
+                    payLoad: Array.isArray(data) ? data : data.cities,
+                });
+            } catch (error) {
+                dispatch({ type: "rejected", payLoad: error.message });
             }
         }
 
-        fetchCities();
+        getCities();
     }, []);
 
-    const getCity = useCallback(
-        async function getCity(id) {
-
-            // if request for same current city
-            // console.log(id, currentCity.id, (id) == currentCity.id)
-            if (id == currentCity.id) return;
-
-            dispatch({ type: "loading" });
-            try {
-                const res = await fetch(`${BASE_URL}/cities/${id}`);
-                const data = await res.json();
-                dispatch({
-                    type: "city_loaded",
-                    payload: data
-                })
-
-            } catch (e) {
-                dispatch({
-                    type: "rejected",
-                    payload: "there was an error fetching...."
-                })
-            }
-        },
-        [currentCity.id])
+    const getCity = useCallback(async function getCity(id) {
+        dispatch({ type: "loading" });
+        try {
+            const res = await fetch(
+                `https://worldwise-one-tau.vercel.app/api/cities?id=${id}`
+            );
+            if (!res.ok) throw new Error("Something went wrong");
+            const data = await res.json();
+            dispatch({ type: "city/loaded", payLoad: data });
+        } catch (error) {
+            dispatch({ type: "rejected", payLoad: error.errorMsg });
+        }
+    }, []);
 
     async function createCity(newCity) {
         dispatch({ type: "loading" });
         try {
-
-            const res = await fetch(`${BASE_URL}/cities`, {
-                method: 'POST',
+            const res = await fetch(`https://worldwise-one-tau.vercel.app/api/cities`, {
+                method: "POST",
                 body: JSON.stringify(newCity),
                 headers: {
-                    "Content-Type": "application/json"
-                }
+                    "Content-Type": "application/json",
+                },
             });
+            if (!res.ok) throw new Error("Something went wrong");
             const data = await res.json();
-            // setCities((cities) => [...cities, data]);
-            dispatch({
-                type: "city_created",
-                payload: data
-            })
-
-        } catch (e) {
-            dispatch({ type: "rejected", payload: "there was an error adding...." });
+            dispatch({ type: "city/created", payLoad: data });
+        } catch (error) {
+            dispatch({ type: "rejected", payLoad: error.errorMsg });
         }
     }
 
-    async function deleteCity(cityId) {
+    async function deleteCity(id) {
         dispatch({ type: "loading" });
         try {
-
-            await fetch(`${BASE_URL}/cities/${cityId}`, {
-                method: 'DELETE'
-            });
-
-            dispatch({
-                type: "cities_deleted",
-                payload: cityId
-            })
-
+            const res = await fetch(
+                `https://worldwise-one-tau.vercel.app/api/cities?id=${id}`,
+                {
+                    method: "DELETE",
+                }
+            );
+            if (!res.ok) throw new Error("Something went wrong");
+            dispatch({ type: "city/deleted", payLoad: id });
         } catch (error) {
-            dispatch({ type: "rejected", payload: "there was an error deleting..." });
+            dispatch({ type: "rejected", payLoad: error.errorMsg });
         }
     }
 
-    return <CitiesContext.Provider value={
-        {
-            cities, isLoading, getCity, currentCity, createCity, deleteCity, error
-        }
-    }>
-        { children }
-    </CitiesContext.Provider>
+    return (
+        <citiesContext.Provider
+            value={ {
+                cities,
+                isLoading,
+                curCity,
+                getCity,
+                createCity,
+                deleteCity,
+                errorMsg,
+            } }
+        >
+            { children }
+        </citiesContext.Provider>
+    );
 }
 
-// eslint-disable-next-line react-refresh/only-export-components
-export function useCities() {
-    const context = useContext(CitiesContext);
-
-    if (context === undefined) throw new Error("CitiesContext is used outside CitiesProvider");
-
+function useCities() {
+    const context = useContext(citiesContext);
+    if (context === undefined)
+        throw new Error("CitiesContext was used outside the CitiesProvider");
     return context;
 }
 
-// export {
-//     CitiesProvider
-// }
+export { CitiesProvider, useCities };
+
+CitiesProvider.propTypes = {
+    children: PropTypes.any,
+};
