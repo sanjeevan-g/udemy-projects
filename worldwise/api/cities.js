347d574f05853d6270457fd9f45ab57880a96150
sanjeevan <s.y.gunasekaran@accenture.com>
2024-08-23 12:37:50 +0530

    test

--- worldwise/api/cities.js
+++ worldwise/api/cities.js
@@ -60,7 +60,7 @@ export default function handler(req, res) {
     const method = req.method;
     const { id } = req.query;
 
-    const filePath = path.join("src",'data',"cities.json");
+    const filePath = path.join("cities.json");
     const writePath = path.join("/tmp", "cities.json");
 
     // Read the JSON file
