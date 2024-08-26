d4d1cf01c0f7f41b1139bb04e627a286c676ebac
sanjeevan <s.y.gunasekaran@accenture.com>
2024-08-26 12:01:39 +0530

    component optimization

--- workout-timer/src/Calculator.js
+++ workout-timer/src/Calculator.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { memo, useEffect, useState } from 'react';
 import clickSound from './ClickSound.m4a';
 
 function Calculator({ workouts, allowSound }) {
@@ -7,9 +7,15 @@ function Calculator({ workouts, allowSound }) {
   const [speed, setSpeed] = useState(90);
   const [durationBreak, setDurationBreak] = useState(5);
 
-  const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
+  const [duration, setDuration] = useState(0);
+
+  // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
   const mins = Math.floor(duration);
   const seconds = (duration - mins) * 60;
+
+  useEffect(() => {
+    setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak)
+  }, [number, sets, speed, durationBreak])
 
   const playSound = function () {
     if (!allowSound) return;
@@ -17,17 +23,26 @@ function Calculator({ workouts, allowSound }) {
     sound.play();
   };
 
+  function handleInc() {
+    setDuration((prev) => Math.floor(prev) + 1)
+  }
+
+  function handleDec() {
+    setDuration(prev => prev > 0 ? Math.ceil(prev) - 1 : 0)
+  }
+
+
   return (
     <>
       <form>
         <div>
           <label>Type of workout</label>
-          <select value={number} onChange={(e) => setNumber(+e.target.value)}>
-            {workouts.map((workout) => (
-              <option value={workout.numExercises} key={workout.name}>
-                {workout.name} ({workout.numExercises} exercises)
+          <select value={ number } onChange={ (e) => setNumber(+e.target.value) }>
+            { workouts.map((workout) => (
+              <option value={ workout.numExercises } key={ workout.name }>
+                { workout.name } ({ workout.numExercises } exercises)
               </option>
-            ))}
+            )) }
           </select>
         </div>
         <div>
@@ -36,10 +51,10 @@ function Calculator({ workouts, allowSound }) {
             type='range'
             min='1'
             max='5'
-            value={sets}
-            onChange={(e) => setSets(e.target.value)}
+            value={ sets }
+            onChange={ (e) => setSets(e.target.value) }
           />
-          <span>{sets}</span>
+          <span>{ sets }</span>
         </div>
         <div>
           <label>How fast are you?</label>
@@ -48,10 +63,10 @@ function Calculator({ workouts, allowSound }) {
             min='30'
             max='180'
             step='30'
-            value={speed}
-            onChange={(e) => setSpeed(e.target.value)}
+            value={ speed }
+            onChange={ (e) => setSpeed(e.target.value) }
           />
-          <span>{speed} sec/exercise</span>
+          <span>{ speed } sec/exercise</span>
         </div>
         <div>
           <label>Break length</label>
@@ -59,23 +74,23 @@ function Calculator({ workouts, allowSound }) {
             type='range'
             min='1'
             max='10'
-            value={durationBreak}
-            onChange={(e) => setDurationBreak(e.target.value)}
+            value={ durationBreak }
+            onChange={ (e) => setDurationBreak(e.target.value) }
           />
-          <span>{durationBreak} minutes/break</span>
+          <span>{ durationBreak } minutes/break</span>
         </div>
       </form>
       <section>
-        <button onClick={() => {}}>–</button>
+        <button onClick={ handleDec }>–</button>
         <p>
-          {mins < 10 && '0'}
-          {mins}:{seconds < 10 && '0'}
-          {seconds}
+          { mins < 10 && '0' }
+          { mins }:{ seconds < 10 && '0' }
+          { seconds }
         </p>
-        <button onClick={() => {}}>+</button>
+        <button onClick={ handleInc }>+</button>
       </section>
     </>
   );
-}
+};
 
-export default Calculator;
+export default memo(Calculator);
