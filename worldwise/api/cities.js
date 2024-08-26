93043c8d28fc4b158c19d5610d1be2040ca1e649
sanjeevan <s.y.gunasekaran@accenture.com>
2024-08-26 12:43:43 +0530

    update

--- worldwise/api/cities.js
+++ worldwise/api/cities.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 
-const filePath = path.join(process.cwd(), "src", "data", "cities.json");
+const filePath = path.join(__dirname, "src", "data", "cities.json");
 const writePath = path.join("/tmp", "cities.json");
 
 export default function handler(req, res) {
